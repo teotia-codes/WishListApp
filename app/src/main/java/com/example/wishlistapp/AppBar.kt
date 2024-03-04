6e3a507af719bcd87e7a7b47d22e59fb013b2505
teotia-codes <priyanshu.teotia50@gmail.com>
2024-03-04 19:31:43 +0530

    Intial

---
+++ app/src/main/java/com/example/wishlistapp/AppBar.kt
@@ -0,0 +1,36 @@
+package com.example.wishlistapp
+
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun AppBarView(
+  title : String,
+  onBackNavClicked: () -> Unit = {}
+){
+    val navigationIcon : (@Composable () -> Unit)? ={
+        IconButton(onClick = { onBackNavClicked() }) {
+           Icon(imageVector = Icons.AutoMirrored.Filled.ArrowBack, contentDescription = null)
+        }
+    }
+
+   TopAppBar(title= {Text(text = title, color = colorResource(id = R.color.black),
+       modifier = Modifier
+           .padding(start = 4.dp)
+           .heightIn(max = 24.dp))},
+       elevation = 3.dp,
+       backgroundColor = Color(0xFFBDFFEC),
+       navigationIcon =
+   )
+}
